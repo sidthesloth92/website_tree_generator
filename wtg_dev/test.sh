7c09bbac4c5d4e75cbf6000a8f4d3f8bd1612c9c
dineshbalaji <www.sidthesloth@gmail.com>
2015-06-26 12:57:19 +0530

    initial commit

---
+++ wtg_dev/test.sh
@@ -0,0 +1,6 @@
+./webtemp_local 1_web_noreset_noscss
+./webtemp_local -r 2_web_reset_noscss
+./webtemp_local -r -s 3_web_reset_scss
+./webtemp_local -m 4_mobile_noreset_noscss
+./webtemp_local -m -r 5_mobile_reset_noscss
+./webtemp_local -m -r -s 6_mobile_reset_scss
